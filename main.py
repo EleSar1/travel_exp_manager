a79578483033c2a0ad209b98f7daa24a3a5e72f9
EleSar <sarnicola.eleonora@gmail.com>
2025-01-16 11:29:56 +0100

    add loop to collect daily expenses

--- main.py
+++ main.py
@@ -3,9 +3,17 @@
 travel_days = int(input("How many days will your trip last? "))
 budget = float(input("How much is your budget for this trip? $"))
 
-expenses_for_category = {"Lodging": [0 for list_length in range(travel_days)],
-                         "Meals": [0 for list_length in range(travel_days)],
-                         "Transport": [0 for list_length in range(travel_days)],
-                         "Other": [0 for list_length in range(travel_days)]}
+expenses_for_category = {"Lodging": [0] * travel_days,
+                         "Meals": [0] * travel_days,
+                         "Transport": [0] * travel_days,
+                         "Other": [0] * travel_days}
 
 
+
+for day in range(travel_days):
+    for category in expenses_for_category.keys():
+        expense = input(f"{category} day {day+1}: ")
+        expenses_for_category[category][day] = float(expense)
+
+        
+print(expenses_for_category.items())
