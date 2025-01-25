b0687b0196f5af35e01cf0fac15b4499f58d74bb
EleSar <sarnicola.eleonora@gmail.com>
2025-01-25 10:43:53 +0100

    add budget check with condition

--- main.py
+++ main.py
@@ -34,3 +34,28 @@ if grandtotal.is_integer():
     print(f"Grandtotal: {int(grandtotal)}")
 else: 
     print(f"Grandtotal: {grandtotal:.2f}")
+
+
+if budget < grandtotal: 
+    print("Your expenses exceed the entered budget.")
+else: 
+    print("You are staying within the set budget!")
+
+
+choice = input("Do you want to change any expense?  Y/N\n")
+if choice == "Y":
+    print("Choose the category:")
+    category_choice = int(input("Press [1] if you want to change Lodging\n[2] if you want to change Meals\n[3] if you want to change Transport\n[4] if you want to change Other\n. . . "))
+    while choice == "Y":
+        if category_choice == 1:
+            day_to_change = int(input("Please select the day: "))
+            if day_to_change > day or day_to_change < 1:
+                print("Error.")
+            else:
+                new_expense = float(input(f"Please enter the new expense for day {day_to_change}: $"))
+                expenses_for_category["Lodging"][day_to_change-1] = new_expense
+        choice = input("Do you want to change anything else? Y/N\n")
+    print(new_expense)
+    print(expenses_for_category)
+else:
+    print("Goodbye!")
