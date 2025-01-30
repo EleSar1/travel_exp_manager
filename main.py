c76ba4a1379c6d5882a7024ca1424243d0ef3a94
EleSar <sarnicola.eleonora@gmail.com>
2025-01-30 10:20:57 +0100

    add docstring to get_travel_details function

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 
 def get_travel_details():
     
+    """
+    Prompts the user to input the number of travel days and the total budget.  
+    Ensures valid input by handling errors and enforcing positive values.
+
+    Returns:
+        tuple: (int, float)  
+        - travel_days: The number of days for the trip (must be at least 1).  
+        - budget: The total budget for the trip (must be non-negative).  
+    """
+
     travel_days = 0
     while travel_days < 1:
-        try:
-            travel_days = int(input("How many days will your trip last? "))
+        try: 
+            travel_days = int(input("How many days will your trip lasts? "))
             if travel_days < 1:
                 print("The number of days must be at least 1.")
         except ValueError:
@@ -15,28 +25,51 @@ def get_travel_details():
         try:
             budget = float(input("How much is your budget for this trip? $"))
             if budget < 0:
-                print("The budget cannot be negative.")
+                print("The budget cannot be negative.")         
         except ValueError:
             print("Wrong value. Please enter a valid number.")
-
+    
     return travel_days, budget
-            
-
-def initialize_expenses(travel_days):
-
-    expense_for_category = {"Lodging": [0] * travel_days,  
-                            "Meals": [0] * travel_days,
-                            "Transport": [0] * travel_days,
-                            "Other": [0] * travel_days}
-    return expense_for_category
 
 
-def collect_expenses(travel_days, expense_for_category):
+def initialize_expenses():
+
+    pass
+
+
+def collect_expenses():
+    
+    pass
+
+
+def calculate_category_totals():
+    
+    pass
+
+
+def calculate_grandtotal():
+    
+    pass
+
+
+def display_totals():
+
+    pass
+
+
+def check_budget():
+
+    pass
+
+
+def modify_expenses():
+
     pass
 
 
 def main():
-    travel_days, budget = get_travel_details()
+    
+    pass
 
 
 if __name__ == "__main__":
